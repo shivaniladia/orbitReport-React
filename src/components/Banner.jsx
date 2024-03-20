182d1c7e77c89a35095d52b33d8bb69bad90f092
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-03-20 10:54:02 -0500

    modified:   src/App.jsx 	new file:   src/components/Banner.jsx 	modified:   src/components/Buttons.jsx 	modified:   src/components/Table.jsx 	deleted:    vite.config.js

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styling.css";
+
+function Banner() {
+    return (
+        <div>
+            <header>
+                <h1>Orbit Report</h1>
+            </header>
+            <p>Click on the buttons to see the satellites in that orbit type.</p>
+        </div>
+    );
+}
+
+export default Banner;
